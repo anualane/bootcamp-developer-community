0f37ca7edbb1c1e8a8bf450b15bff1b9cdb6cc1d
anualane <anualane@in.ibm.com>
2022-12-11 18:48:55 +0530

    Firts Commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useEffect } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Routes from "./components/routing/Routes";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, [loadUser]);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
